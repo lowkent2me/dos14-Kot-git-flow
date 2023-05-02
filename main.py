b717379d7f315df7c4f9a1c502eb27a07c1bb41c
Kot <vitalikot1996@gmail.com>
2023-05-02 15:05:09 +0300

    Выполняется задание 17 (написание функции кредита: чтение и запись файла account)

--- main.py
+++ main.py
@@ -1,49 +1,44 @@
-# import os
-import re
+# Lesson17
+import json
+import yaml
+import csv
+# Чтение файлов и составление словарей
+with open('./data/credit.json', 'r') as open_credit:
+    read_credit = open_credit.read()
+    credit_d = json.loads(read_credit)
+with open('./data/deposit.yaml', 'r') as open_deposit:
+    read_deposit = open_deposit.read()
+    deposit_d = yaml.load(read_deposit, Loader=yaml.FullLoader)
+with open('./data/account.csv', 'r') as open_account:
+    reader_account = csv.DictReader(open_account, delimiter=',')
+    account_d = []
+    for row in reader_account:
+        account_d.append(row)
+# Создать функцию расчёта кредитов
+# Добавляем sum к сумме на счёте клиента. Вычитая со счёта банка (id=0)
+# Пишем новые суммы в файл account.csv
+# Расчитывем сумму которую надо списывать в месяц
+# для этого итоговую сумму (сложный процент) делим на term*12
+# Каждый месяц списываем сумму со счёта. Добавляя на счёт банка (id=0)
+# Пишем новые суммы в файл account.csv
+# Если деньги закончились пишем "Дорогой клиент, погасите ваш кредит. Сумма задолжености "
+print(account_d)
+for cr in credit_d: #находим запросы на получение кредита
+    if cr['sum'] == 0:
+        continue
+    else:
+        for cr_p in account_d: #инициируем процедуру перечисления средств
+            if int(cr['id']) == int(cr_p['id']):
+                print('аккаунт ', cr_p['id'], ' взял кредит на сумму', cr['sum'])
+                account_d[0]['amount'] = int(account_d[0]['amount']) - int(cr['sum'])
+                cr_p['amount'] = int(cr_p['amount']) + int(cr['sum'])
+            else:
+                continue
+with open('./data/account.csv', 'w') as open_account: # Запись зменений в account.csv
+    write_account = csv.DictWriter(open_account, fieldnames=list(account_d[0].keys()))
+    write_account.writeheader()
+    for row in account_d:
+        write_account.writerow(row)
+print(account_d)
 
-# Lesson 15
-# shell_name = os.environ["SHELL"]
-# if shell_name == "/bin/bash":
-#     print("Greetings BASH")
-# else:
-#     print("Hello " + shell_name[5:])
 
-# Lesson 16
-# Решение
-# Формирование списка словарей
-start_sum = ["1_1000", "2_30000", "3_100000", "8_100", "5_11111", "9_14124124124", "6_444", "4_123456",
-             "7_100000000000", "10_81214"]
-rate = ["1_10", "2_11", "3_8", "4_13", "5_11", "6_6", "7_9", "8_11", "9_13", "10_12"]
-term = ["1_1", "2_2", "3_2", "4_6", "5_8", "6_20", "7_9", "8_11", "9_13", "10_12"]
-start_sum.sort(); start_sum.append(start_sum[0]); start_sum.pop(0)
-n = 0
-idn_list = []
-for n in start_sum:
-    # print(n + '\n')
-    idn = re.match(r'\d\d?\d?', n).group(0)
-    # print(x + '\n')
-    exec("idn_dict_%s = {'id': int(re.split(r'_', n)[0]), 'start_sum': int(re.split(r'_', n)[1])}" % idn)
-    exec('idn_list.append(idn_dict_%s)' % idn)
-for n in rate:
-    idn = re.match(r'\d\d?\d?', n).group(0)
-    exec("idn_dict_%s.update(rate = int(re.split(r'_', n)[1]))" % idn)
-for n in term:
-    idn = re.match(r'\d\d?\d?', n).group(0)
-    exec("idn_dict_%s.update(term = int(re.split(r'_', n)[1]))" % idn)
-# расчёт сложного процента
-def pow(x,y):
-    i = 1
-    power = 1
-    for i in range(y):
-        power = power * x
-        i += i
-    return power
-percent = lambda f_sum, f_perc: f_sum * f_perc
-for d_perc in idn_list:
-    st_sum = d_perc['start_sum']
-    perc = 1 + d_perc['rate'] / 100
-    prof = pow(perc, d_perc['term'])
-    raw_sum = percent(st_sum, prof)
-    end_sum = float('{:.2f}'.format(raw_sum))
-    d_perc.update(end_sum=float(end_sum))
-print(idn_list)
